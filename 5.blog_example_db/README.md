9ca6fa0ca212bcfada8a62917bda196145546a0a
Sonat <80075444+sonatipek@users.noreply.github.com>
2023-07-21 10:25:19 +0300

    Add files via upload

---
+++ 5.blog_example_db/README.md
@@ -0,0 +1,13 @@
+# What can you learn?
+- Mysql
+
+
+# Usage
+- creatre .env file
+- copy the following and fill in the "..." part yourself
+```
+DB_USER="..."
+DB_PASSWORD="..."
+DB_HOST="..."
+DB_NAME=".."
+```
---
+++ 5.blog_example_db/routes/admin.js
@@ -0,0 +1,69 @@
+// Requirements
+const express = require('express');
+
+
+// Create router
+const router = express.Router();
+
+// !Data
+const data = {
+    categories: ["Art", "Tech", 'Software'],
+    blogs: [
+        {
+            blogid: 1,
+            title: "Art History",
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quos molestias quo, excepturi voluptatem labore.",
+            img: "1.jpg",
+            isShownOnHome: false,
+            category: "Art"
+        },
+        {
+            blogid: 2,
+            title: "Windows 14 Features",
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quos molestias quo, excepturi voluptatem labore.",
+            img: "2.jpg",
+            isShownOnHome: false,
+            category: "Tech"
+        },
+        {
+            blogid: 3,
+            title: "Node.js vs PHP",
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quos molestias quo, excepturi voluptatem labore.",
+            img: "3.jpg",
+            isShownOnHome: true,
+            category: "Software"
+
+        },
+        {
+            blogid: 4,
+            title: "How Can I Learn Software",
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi quos molestias quo, excepturi voluptatem labore.",
+            img: "4.jpg",
+            isShownOnHome: true,
+            category: "Software"
+        }
+    ],
+
+    homepage: {
+        title: "My Featured Blog Posts",
+    }
+};
+
+router.use('/blog/create', (req, res) => {
+    res.render('admin/blog-create');
+});
+
+router.use('/blogs/:blogid', (req, res) => {
+    res.render('admin/blog-edit');
+});
+
+router.use('/blogs', (req, res) => {
+    res.render('admin/blog-list');
+});
+
+
+router.use('', (req, res) => {
+    res.render('admin/dashboard');
+});
+
+module.exports=router;
